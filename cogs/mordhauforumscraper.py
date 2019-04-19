82283cff60f042d14688cbf3783ef506a45d0d28
crypdos <45307738+crypdos@users.noreply.github.com>
2019-04-19 12:55:07 +0200

    mordhau forum scraper

---
+++ cogs/mordhauforumscraper.py
@@ -0,0 +1,82 @@
+from bs4 import BeautifulSoup
+from discord.ext import commands
+from datetime import datetime
+import aiohttp
+import threading
+
+class MordhauForumScraper:
+
+    def __init__(self, bot):
+        self.bot=bot
+
+    async def remove_quotes(self, ctx, soup):
+        "Remove quotes"
+        for child in soup.find_all('blockquote'):
+            child.decompose()
+
+
+    async def remove_polls(self, ctx, soup):
+        "Remove polls"
+        for child in soup.find_all(class_='comment-poll'):
+            child.decompose()
+
+
+    async def comment_to_db(self, ctx, comment):
+        "Parse the html for each comment and add relevant information to the db"
+        comment_id = comment.get('data-pk')
+        if await self.bot._db['mordhauforum'].find_one({"comment_id": comment_id}):
+            return # this comment is already added to the db
+        try:
+            user_id = comment.find(class_='username').get('href').split('/')[3]
+            content = comment.find(class_='comment-text').get_text().strip()
+            user_name = comment.find(class_='username').get_text().strip()
+            timestamp = comment.find(class_='comment-date').find(class_='text-muted').get('data-date')[:-6]
+            timestamp = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f")
+            #print(timestamp, comment_id, user_id, user_name, content)
+            self.bot._db['mordhauforum'].insert_one({"comment_id": comment_id, "author_name": user_name, "user_id": user_id, "timestamp": timestamp,
+                               "content": content})
+        except:
+            #print(f"Comment {comment_id} is invalid")
+            return
+
+
+
+    async def process_page(self, ctx, comment_id):
+        """Each comment links to the threadpage it is posted in, process all comments in this threadpage
+        Returns response status"""
+        base_url = "https://mordhau.com/forum/comment/"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(base_url + str(comment_id) + '/find') as response:
+                if response.status != 200:
+                    return response.status
+                comment_html = await response.text()
+        soup = BeautifulSoup(comment_html, 'html.parser')
+        await self.remove_quotes(ctx, soup)
+        await self.remove_polls(ctx, soup)
+        comments = soup.find_all(class_='comment')
+        for comment in comments:
+            await self.comment_to_db(ctx, comment)
+        return response.status
+
+
+    @commands.command(name="mordhauscrape")
+    async def scrape(self, ctx, i :int, increment: int):
+        print(f"Starting mordhauforum scrape, i={i}, increment={increment}")
+        error_buffer = 0
+        #i = 37100
+        #max = 187272
+        while error_buffer < 100:
+            response = await self.process_page(ctx, i)
+            if response != 200:
+                error_buffer += 1
+            elif error_buffer > 0:
+                error_buffer -= 1
+            if i% 50 == 0:
+                print(f" i = {i}")
+            i += increment
+        print(f"Done looping, error buffer = {error_buffer}")
+
+
+
+def setup(bot):
+    bot.add_cog(MordhauForumScraper(bot))
--- utils/textmodel.py
+++ utils/textmodel.py
@@ -13,33 +13,32 @@ class TextModel:
         if self.busy:
             raise Busy
         self.busy=True
-        if self.authorid != authorid:
-            self.name = name
-            self.authorid = authorid
-            wordlist = ""
-            for (coll, serverid) in self.bot._cfg.items('scrapeservers'):
-                query = self.bot._db[coll].find({"author_id" : authorid}, {"_id":0, "content":1})
-                async for doc in query:
-                    wordlist += doc['content'] + '\n'
-            self.model = markovify.NewlineText(wordlist)
-            print("Done creating model")
-            self.busy=False
-        else:
-            self.busy=False
+        if self.authorid == authorid:
+            self.busy = False
             raise ModelAlreadyExists
+        self.name = name
+        self.authorid = authorid
+        wordlist = ""
+        for (coll, serverid) in self.bot._cfg.items('scrapeservers'):
+            query = self.bot._db[coll].find({"author_id" : authorid}, {"_id":0, "content":1})
+            async for doc in query:
+                wordlist += doc['content'] + '\n'
+        self.model = markovify.NewlineText(wordlist)
+        print("Done creating model")
+        self.busy=False
+
 
     async def makeSentence(self, beginning = None):
-        if self.model:
-            if beginning:
-                try:
-                    sentence = self.model.make_sentence_with_start(beginning, tries=1000)
-                    if sentence:
-                        return sentence
-                    else:
-                        return self.model.make_sentence(tries=1000)
-                except KeyError: # beginword not found
-                    return self.model.make_sentence(tries=1000)
+        if not self.model:
+            raise ModelDoesntExist
+        if not beginning:
+            return self.model.make_sentence(tries=1000)
+        try:
+            sentence = self.model.make_sentence_with_start(beginning, tries=1000)
+            if sentence:
+                return sentence
             else:
                 return self.model.make_sentence(tries=1000)
-        else:
-            raise ModelDoesntExist
+        except KeyError: # beginword not found
+            return self.model.make_sentence(tries=1000)
+
